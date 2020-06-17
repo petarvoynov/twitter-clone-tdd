9e14a3934bdc09f5f696d4127d73aa192112ab25
Petar Voynov <ksihonm@abv.bg>
2020-06-17 22:53:06 +0300

    Added Foreign Key Constraints on tweets table

--- database/migrations/2020_06_16_202442_create_tweets_table.php
+++ database/migrations/2020_06_16_202442_create_tweets_table.php
@@ -18,6 +18,8 @@ class CreateTweetsTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->text('body');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
