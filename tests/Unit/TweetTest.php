5adf8450380007129cce1aa3fd80aa19c9ae7e1a
Petar Voynov <ksihonm@abv.bg>
2020-06-22 22:58:01 +0300

    A tweet has many comments

--- tests/Unit/TweetTest.php
+++ tests/Unit/TweetTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Database\Eloquent\Collection;
 use Tests\TestCase;
 
 class TweetTest extends TestCase
@@ -16,5 +17,13 @@ class TweetTest extends TestCase
         $tweet = factory('App\Tweet')->create();
 
         $this->assertInstanceOf('App\User', $tweet->user);
+    }
+
+    /** @test */
+    function a_tweet_has_many_comments()
+    {
+        $tweet = factory('App\Tweet')->create();
+
+        $this->assertInstanceOf(Collection::class, $tweet->comments);
     }
 }
