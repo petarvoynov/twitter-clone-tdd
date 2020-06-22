de63d3a05b04c46cadf96ca5891099d17e49291d
Petar Voynov <ksihonm@abv.bg>
2020-06-22 23:10:58 +0300

    Fixed typo in TweetCommentsTest

--- tests/Feature/TweetCommentsTest.php
+++ tests/Feature/TweetCommentsTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class TweetCommnetsTest extends TestCase
+class TweetCommentsTest extends TestCase
 {
     use RefreshDatabase;
 
