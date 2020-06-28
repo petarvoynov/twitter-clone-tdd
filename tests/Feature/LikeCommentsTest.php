fbeedce6fa716dbf181bf9ef49afd0b47b22985e
Petar Voynov <ksihonm@abv.bg>
2020-06-28 16:10:18 +0300

    Changed the name of the test LikesTest

--- tests/Feature/LikeCommentsTest.php
+++ tests/Feature/LikeCommentsTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class LikesTest extends TestCase
+class LikeCommentsTest extends TestCase
 {
     use RefreshDatabase;
 
