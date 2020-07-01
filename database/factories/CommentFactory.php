21156ef31a4a4e74fd2ccea0acaebe2a45e48000
Petar Voynov <ksihonm@abv.bg>
2020-07-01 13:25:29 +0300

    Added db:seed code

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -9,7 +9,7 @@ $factory->define(Comment::class, function (Faker $faker) {
     return [
         'user_id' => factory('App\User'),
         'commentable_id' => factory('App\Tweet'),
-        'commentable_type' => get_class(factory('App\Tweet')),
+        'commentable_type' => get_class(factory('App\Tweet')->create()),
         'body' => $faker->sentence
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $users = ['test1', 'test2', 'test3', 'test4', 'test5'];
+
+        foreach($users as $user) {
+            factory('App\User')->create([
+                'name' => ucfirst($user),
+                'email' => $user.'@abv.bg',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ]);
+        }
+
+        factory('App\Comment',10)->create();
+
     }
 }
