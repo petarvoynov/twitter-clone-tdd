7a95b50e6045505decf967bf77411210509eba2e
Petar Voynov <ksihonm@abv.bg>
2020-07-12 14:01:50 +0300

    Removed the relationship "Retweet has morphMany comments"

--- app/Retweet.php
+++ app/Retweet.php
@@ -8,11 +8,6 @@ class Retweet extends Model
 {
     protected $guarded = [];
 
-    public function comments()
-    {
-        return $this->morphMany('App\Comment', 'commentable');
-    }
-
     public function tweet()
     {
         return $this->belongsTo('App\Tweet');
