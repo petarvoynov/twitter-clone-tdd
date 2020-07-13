1a280f724aac28378bce8963837c4bf7e053f72d
Petar Voynov <ksihonm@abv.bg>
2020-07-13 22:51:21 +0300

    An activity can be morphed to comment

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Database\Eloquent\Collection;
 use Tests\TestCase;
 use App\Like;
+use App\Comment;
 
 class ActivityTest extends TestCase
 {
@@ -21,5 +22,16 @@ class ActivityTest extends TestCase
         ]);
 
         $this->assertInstanceOf(Like::class, $activity->subject);
+    }
+
+    /** @test */
+    function an_activity_can_be_morphed_to_comment()
+    {
+        $activity = factory('App\Activity')->create([
+            'subject_id' => factory('App\Comment'),
+            'subject_type' => 'App\Comment'
+        ]);
+
+        $this->assertInstanceOf(Comment::class, $activity->subject); 
     }
 }
