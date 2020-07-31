a9753f0a04c5a571a6eb454043d6dff03647e92f
Petar Voynov <ksihonm@abv.bg>
2020-07-31 21:42:42 +0300

    Rename of a test

--- tests/Feature/AddProfilePictureTest.php
+++ tests/Feature/AddProfilePictureTest.php
@@ -9,7 +9,7 @@ use Tests\TestCase;
 class AddProfilePictureTest extends TestCase
 {
     /** @test */
-    function only_authenticated_users_can_add_profile_picture()
+    function guests_cannot_add_a_profile_picture()
     {
         $this->post('/users/1/profile-picture', [])
             ->assertRedirect('login');
