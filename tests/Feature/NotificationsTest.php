c5e3f3dd0437cffc2e2004bb21bae83e4792807e
Petar Voynov <ksihonm@abv.bg>
2020-08-08 15:54:20 +0300

    Moved the tests for the notification in their NotificationsTest class

---
+++ tests/Feature/NotificationsTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class NotificationsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function an_authenticated_user_can_subscribe_for_notifications_about_tweets_being_created_by_a_user_he_follows()
+    {
+        $this->withoutExceptionHandling();
+        // Given we are sign in and follow a user
+        $user = $this->signIn();
+        $userToFollow = factory('App\User')->create();
+
+        $user->follow($userToFollow);
+
+        // When we hit the route to subscribe for notifications for this user's tweets
+        $this->post("/users/{$userToFollow->id}/subscribe");
+
+        // Then there should be a record in the database
+        $this->assertDatabaseHas('subscriptions', [
+            'user_id' => $user->id,
+            'subscribed_to' => $userToFollow->id
+        ]);
+    }
+
+    /** @test */
+    function an_authenticated_user_can_unsubscribe_for_notifications()
+    {
+        // Given we are sing in and follow and subscribe to a user
+        $user = $this->signIn();
+
+        $userToFollowAndSubscribe = factory('App\User')->create();
+
+        $user->follow($userToFollowAndSubscribe);
+
+        $user->subscribe($userToFollowAndSubscribe);
+        
+        // When we hit the route to unsubscribe for the user
+        $this->delete("/users/{$userToFollowAndSubscribe->id}/unsubscribe");
+
+        // Then there shouldn't be a record in the database
+        $this->assertDatabaseMissing('subscriptions', [
+            'user_id' => $user->id,
+            'subscribed_to' => $userToFollowAndSubscribe->id
+        ]);
+    }
+
+    /** @test */
+    function an_authenticated_user_receives_a_notification_when_a_user_that_he_follows_create_a_tweet()
+    {
+        // Given we are sing in and follow and subcribe to a user
+        $user = factory('App\User')->create();
+
+        $userToFollowAndSubscribe = factory('App\User')->create();
+
+        $user->follow($userToFollowAndSubscribe);
+
+        $user->subscribe($userToFollowAndSubscribe);
+
+        // When this user creates a tweet
+        $this->actingAs($userToFollowAndSubscribe)->post('/tweets', [
+            'body' => 'This tweet should generate a notification'
+        ]);
+
+        // Then there should be a record in the database that we have a notification
+        $this->assertDatabaseHas('notifications',[
+            'notifiable_id' => $user->id
+        ]);
+    }
+}
