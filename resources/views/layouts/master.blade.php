77304e577fd1eb30a6862f25123096c38e5affea
Petar Voynov <ksihonm@abv.bg>
2020-09-07 17:07:06 +0300

    Added logout button and the end of the menu instead of "more" option

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -65,8 +65,10 @@
                             </a>
                         </li>
                         <li class="menu-item mt-2">
-                            <a href="#">
-                                <svg style="width: 28px; margin-right: 16px;" viewBox="0 0 20 20" fill="currentColor" class="dots-circle-horizontal w-6 h-6"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd"></path></svg>More
+                            <a href="{{ route('logout') }}" 
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                                <svg style="width: 28px; margin-right: 16px;" viewBox="0 0 20 20" fill="currentColor" class="logout w-6 h-6"><path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path></svg>Logout
                             </a>
                         </li>
                         <li class="mt-2"><div class="button text-center mt-4">Tweet</div></li>
@@ -109,6 +111,11 @@
             </div>
         </div>
     </div>
+
+    {{-- The form to logout --}}
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
     @yield('scripts')
 </body>
 </html>
