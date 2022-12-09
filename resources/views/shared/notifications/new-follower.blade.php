ab27a76271802115e8e38fe69882e31c9bf4f997
Petar Voynov <ksihonm@abv.bg>
2022-12-09 17:00:21 +0200

    Add indication if the notification is read or not.

--- resources/views/shared/notifications/new-follower.blade.php
+++ resources/views/shared/notifications/new-follower.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <a style="width:100%" href="{{ route('users.show', ['user' => $notification->data['user_id'], 'notification_id' => $notification->id]) }}">
-        <div class="col-12 notification alert alert-primary" role="alert">
+        <div class="col-12 notification alert {{ is_null($notification->read_at) ? 'alert-primary' : 'alert-light' }}" role="alert">
             <div>{{ $notification->data['user_name'] }} Followed you!</div>
         </div>
     </a> 
--- resources/views/shared/notifications/tweet-created.blade.php
+++ resources/views/shared/notifications/tweet-created.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <a style="width:100%" href="{{ route('tweets.show', ['tweet' => $notification->data['tweet_id']]) }}">
-        <div id="{{ $notification->data['tweet_id'] }}" class="col-12 notification alert alert-primary" role="alert">
+        <div id="{{ $notification->data['tweet_id'] }}" class="col-12 notification alert {{ is_null($notification->read_at) ? 'alert-primary' : 'alert-light' }}" role="alert">
             <div>
                 {{ $notification->data['tweet_owner_name'] }}<small> tweeted:</small>
             </div>
