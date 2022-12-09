9c77a01794d80378b6403d949f008586dcf409cf
Petar Voynov <ksihonm@abv.bg>
2022-12-09 16:51:53 +0200

    Add missing bracket.

--- resources/views/shared/notifications/unfollowed.blade.php
+++ resources/views/shared/notifications/unfollowed.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <a style="width:100%" href="{{ route('users.show', ['user' => $notification->data['user_id'], 'notification_id' => $notification->id) }}">
+    <a style="width:100%" href="{{ route('users.show', ['user' => $notification->data['user_id'], 'notification_id' => $notification->id]) }}">
         <div class="col-12 notification alert alert-primary" role="alert">
             <div>{{ $notification->data['user_name'] }} unfollowed you!</div>
         </div>
